3835e331e99af44f77dfb73d01a1bf525430ecb3
Osama Malik <usmamalik344@gmail.com>
2022-08-30 22:12:46 +0500

    some changes

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -2,19 +2,28 @@ import React from 'react'
 import {useState , useEffect} from "react"
 import {Box ,  Stack , Typography} from "@mui/material"
 import Sidebar from './Sidebar'
+import Video from './Video'
+import {FetchFromAPI} from "../utils/FetchFromAPI"
+
 
 const Feed = () => {
   return (
-    <Stack sx={{ flexDirection: {sx: "column" , md: "row" , backgroundColor: "black"} } }>
-        <Box  sx={ { height: {sx: "auto" , md: "92vh"  ,} , borderRight: "1px solid #3d3d3d" , px: {sx: 0 , md: 2}  ,}} >
+    <Stack  sx={{  background: "black" , flexDirection: { sx: "column", md: "row"  }}  }>
+        <Box  sx={{ height: { sx: "auto", md: "140vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } ,}} >
 
            <Sidebar />
-         <Typography className="copywrite" variant="body2" sx={{ mt: 1.5 , color: "#fff" }} >
-          Copy Write yess
+         <Typography className="copywrite" variant="body2" sx={{ mt: 1.5 , color: "black" }} >
+          Copy Right By Usama
          </Typography>
         </Box>
 
-
+        <Box p={2} sx={{ overflow: "auto" , height: "90vh" , flex: 2}}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white"}}> 
+        New
+       <span style={{color: "#F31503"}}>  Videos  </span>
+      </Typography>
+      <Video video={[]} />
+      </Box>
     </Stack>
   )
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import {Stack , Button} from "@mui/material"
-import {categories} from "../utils/Constants"
+import { Stack, Button } from "@mui/material"
+import { categories } from "../utils/Constants"
+
+const selectedCategory = "New";
 
 const Sidebar = () => {
   return (
-    <Stack  dicretion=" row" sx={{overflowy: "auto" , height: {sx: "auto" , md: "90%" }, flexDirection: { md: "column"} }}>
-{  categories.map((categorie) =>(
-    <Button>
-    <span> {categorie.name }</span>
-    <span>  {categorie.icon} </span>
-    </Button> 
-)
-    
- ) }
+    <Stack dicretion=" row" sx={{ overflowy: "auto", height: { sx: "auto", md: "90%" }, flexDirection: { md: "column" } }}>
+      {categories.map((category) => (
+        <button className="category-btn" style={{ background: category.name === selectedCategory && "#FC1503" , color: "white"}} key={category.name}>
+          <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>  {category.icon} </span>
+          <span style={{ opacity: category.name ===   selectedCategory ? "1 " : "0.8"}}> {category.name}</span>
+
+        </button>
+      )
+
+      )}
 
     </Stack>
   )
